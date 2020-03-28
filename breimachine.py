28cfd8447e03320b79f879acc87de14971527376
Wouter Oosterveld <wouter@fizzyflux.nl>
2020-03-28 01:39:11 +0100

    add something useful

--- breimachine.py
+++ breimachine.py
@@ -1,23 +1,33 @@
 from flask import Flask, request
 from config import TOKEN
 import telegram
+from telegram.ext import Dispatcher, CommandHandler
+from requests import get
 
 app = Flask(__name__)
 
 bot = telegram.Bot(token=TOKEN)
+dispatcher = Dispatcher(bot, None)
+
+def get_stats_for(target_country="Netherlands"):
+    countries=get("https://api.covid19api.com/summary").json()["Countries"]
+    for country in countries:
+        if country["Country"] == target_country:
+            return country
+    return "Not found"
+        
+        
+    
+def stats(bot, update):
+    update.message.reply_text(str(get_stats_for()))
 
 @app.route('/', methods=['POST'])
 def webhook():
     update = telegram.Update.de_json(request.get_json(force=True), bot)
-
-    chat_id = update.message.chat.id
-
-    # Telegram understands UTF-8, so encode text for unicode compatibility
-    text = update.message.text
-
-    # repeat the same message back (echo)
-    bot.sendMessage(chat_id=chat_id, text=text)
+    dispatcher.process_update(update)
     return 'OK'
+
+dispatcher.add_handler(CommandHandler("stats", stats))
 
 if __name__ == '__main__':
     app.run()
